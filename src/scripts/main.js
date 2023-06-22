1b4727a790c4f6b96308911c49df5ec49578b8b4
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-06-22 16:37:26 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -37,3 +37,18 @@ dropdowns.forEach(dropdown => {
     });
   });
 });
+
+const button = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    button.addClass('show');
+  } else {
+    button.removeClass('show');
+  }
+});
+
+button.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
